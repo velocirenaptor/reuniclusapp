af42cb28c489c6be345a72245932c8d5b2cf66cc
ren0 <renomg.194@gmail.com>
2024-10-08 12:51:56 -0600

    app

---
+++ android/app/src/main/kotlin/com/adsofsito/reuniclus_leju/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.adsofsito.reuniclus_leju
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/reuniclus_leju/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reuniclus_leju
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
